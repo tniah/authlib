3fb862b6eca3c9dd28521c549c29acbd6395e675
Makai <tronghaibk2008@gmail.com>
2025-03-08 02:09:30 +0700

    Refactor source code

--- models/authorization_code.go
+++ models/authorization_code.go
@@ -4,25 +4,14 @@ import "time"
 
 type AuthorizationCode interface {
 	GetCode() string
-	SetCode(code string)
 	GetClientID() string
-	SetClientID(clientID string)
 	GetUserID() string
-	SetUserID(userID string)
 	GetRedirectURI() string
-	SetRedirectURI(redirectURI string)
 	GetResponseType() string
-	SetResponseType(responseType string)
 	GetScopes() []string
-	SetScopes(scopes []string)
 	GetNonce() string
-	SetNonce(nonce string)
 	GetState() string
-	SetState(state string)
 	GetAuthTime() time.Time
-	SetAuthTime(authTime time.Time)
 	GetCodeChallenge() string
-	SetCodeChallenge(codeChallenge string)
 	GetCodeChallengeMethod() string
-	SetCodeChallengeMethod(codeChallengeMethod string)
 }
