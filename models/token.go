aa4d950d0f34ca558b4742a56f12ea4b49474c5f
Makai <tronghaibk2008@gmail.com>
2025-03-08 02:04:18 +0700

    Add .gitignore

--- models/token.go
+++ models/token.go
@@ -4,23 +4,14 @@ import "time"
 
 type Token interface {
 	GetTokenID() string
-	//SetID(id string)
 	GetAccessToken() string
-	//SetAccessToken(token string)
 	GetRefreshToken() string
-	//SetRefreshToken(token string)
 	GetClientID() string
-	//SetClientID(clientID string)
 	GetType() string
-	//SetTokenType(tokenType string)
 	GetScopes() []string
-	//SetScopes(scopes []string)
 	GetIssuedAt() time.Time
-	//SetIssuedAt(issuedAt time.Time)
 	GetExpiresIn() time.Duration
-	//SetExpiresIn(expiresIn time.Duration)
 	GetUserID() string
-	//SetUserID(userID string)
 	GetData() map[string]interface{}
 	GetExtraData() map[string]interface{}
 }
