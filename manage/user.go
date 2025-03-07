eb8bc46a0d77a91351e504205fbda03153d53217
Makai <tronghaibk2008@gmail.com>
2025-03-07 17:03:02 +0700

    Refactor source code

--- manage/user.go
+++ manage/user.go
@@ -2,11 +2,8 @@ package manage
 
 import (
 	"context"
-	"errors"
 	"github.com/tniah/authlib/models"
 )
-
-var ErrUserNotFound = errors.New("user not found")
 
 type UserStore interface {
 	FetchByID(ctx context.Context, id string) (models.User, error)
@@ -20,15 +17,6 @@ func NewUserManager(store UserStore) *UserManager {
 	return &UserManager{store: store}
 }
 
-func (m *UserManager) GetByID(ctx context.Context, id string) (models.User, error) {
-	u, err := m.store.FetchByID(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-
-	if u == nil {
-		return nil, ErrUserNotFound
-	}
-
-	return u, nil
+func (m *UserManager) GetByID(ctx context.Context, userID string) (models.User, error) {
+	return m.store.FetchByID(ctx, userID)
 }
