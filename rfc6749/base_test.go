e16434a519105fb1353d2e2842e64ad4b664c220
Makai <tronghaibk2008@gmail.com>
2025-03-25 09:26:07 +0700

    Add tests for base grant

--- rfc6749/base_test.go
+++ rfc6749/base_test.go
@@ -6,7 +6,9 @@ import (
 	"github.com/tniah/authlib/errors"
 	"github.com/tniah/authlib/mocks/models"
 	"github.com/tniah/authlib/requests"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestValidateRedirectURI(t *testing.T) {
@@ -42,9 +44,9 @@ func TestValidateRedirectURI(t *testing.T) {
 
 		actual, err := g.ValidateRedirectURI(r, c)
 		assert.ErrorAs(t, err, &authErr)
-		assert.Empty(t, actual)
-		assert.Equal(t, authErr.State, r.State)
-		assert.Equal(t, authErr.Description, v.ErrDescription)
+		assert.Empty(t, actual, "expected=\"\", actual=%s", actual)
+		assert.Equal(t, r.State, authErr.State, "expected=%s, actual=%s", r.State, authErr.State)
+		assert.Equal(t, v.ErrDescription, authErr.Description, "expected=%s, actual=%s", v.ErrDescription, actual)
 	}
 
 	for _, v := range []struct {
@@ -74,4 +76,27 @@ func TestValidateRedirectURI(t *testing.T) {
 		assert.NoError(t, err, "error = %v", err)
 		assert.Equal(t, v.Expected, actual, "expected = %v, actual = %v", v.Expected, actual)
 	}
+}
+
+func TestStandardTokenData(t *testing.T) {
+	token := models.NewMockToken(t)
+	tokenType := "Bearer"
+	accessToken := uuid.NewString()
+	refreshToken := uuid.NewString()
+	expiresIn := time.Minute * 60
+	scopes := []string{"openid", "email"}
+
+	token.EXPECT().GetType().Return(tokenType).Once()
+	token.EXPECT().GetAccessToken().Return(accessToken).Once()
+	token.EXPECT().GetRefreshToken().Return(refreshToken).Once()
+	token.EXPECT().GetScopes().Return(scopes).Once()
+	token.EXPECT().GetAccessTokenExpiresIn().Return(expiresIn).Once()
+
+	g := TokenGrantMixin{}
+	data := g.StandardTokenData(token)
+	assert.Equal(t, tokenType, data[ParamTokeType], "token type: expected=%s, actual=%s", tokenType, data[ParamTokeType])
+	assert.Equal(t, accessToken, data[ParamAccessToken], "access token: expected=%s, actual=%s", accessToken, data[ParamAccessToken])
+	assert.Equal(t, expiresIn.Seconds(), data[ParamExpiresIn], "expires in: expected=%s, actual=%s", expiresIn.Seconds(), data[ParamExpiresIn])
+	assert.Equal(t, refreshToken, data[ParamRefreshToken], "refresh token: expected=%s, actual=%s", refreshToken, data[ParamRefreshToken])
+	assert.Equal(t, strings.Join(scopes, " "), data[ParamScope], "scope: expected=%s, actual=%s", strings.Join(scopes, " "), data[ParamScope])
 }
