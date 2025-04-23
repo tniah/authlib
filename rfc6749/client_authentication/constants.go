f966b744a1e5ab0e783541364673f98e5654764a
Makai <tronghaibk2008@gmail.com>
2025-04-23 14:51:23 +0700

    Fix client authentication

--- rfc6749/client_authentication/constants.go
+++ rfc6749/client_authentication/constants.go
@@ -6,9 +6,3 @@ var (
 	ErrInvalidClient  = errors.New("invalid client")
 	ErrNilClientStore = errors.New("client store is nil")
 )
-
-const (
-	AuthMethodClientSecretBasic = "client_secret_basic"
-	AuthMethodClientSecretPost  = "client_secret_post"
-	AuthMethodNone              = "none"
-)
